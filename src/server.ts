36ddae05035ecc1a16916968ced3801be66ec119
josersn <nettorammos@hotmail.com>
2021-10-11 20:55:07 -0300

    fix: remove tsringe

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ import "reflect-metadata";
 import express from "express";
 import { supplementsRoutes } from "./routes/supplements.routes";
 
-import "./shared/container";
 import "./database";
 
 const server = express();
--- src/shared/container/index.ts
+++
@@ -1,9 +0,0 @@
-import { container } from "tsyringe";
-
-import { ISupplementsRepository } from "../../modules/supplements/repositories/ISupplementsRepository";
-import { SupplementsRepository } from "../../modules/supplements/repositories/implementations/SupplementsRepository";
-
-container.registerSingleton<ISupplementsRepository>(
-    "SupplementsRepository",
-    SupplementsRepository
-)
